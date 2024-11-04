a1c6de97cfbfb6b75bfdaf83b8e677efe7c2e610
vdavdov <vdavdov.work@mail.ru>
2024-11-04 19:42:43 +0400

    Update

--- src/main/java/by/vdavdov/mail/MailService.java
+++ src/main/java/by/vdavdov/mail/MailService.java
@@ -62,7 +62,7 @@ public class MailService {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
-            message.setFrom(email, "JiraLike");
+            message.setFrom(email, "JiraRush");
             String content = getContent(template, params);
             message.setText(content, true);
             message.setSubject(Util.getTitle(content));  // TODO calculate title for group emailing only once
